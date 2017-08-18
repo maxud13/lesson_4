68027cb3a5f02d6c7ca6190ddd6581d27a569809
maxud <mlenon13@gmail.com>
2017-08-18 19:11:54 +0300

    new project

---
+++ lesson_4/lesson_4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lesson_4
+//
+//  Created by maxud on 18.08.17.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
