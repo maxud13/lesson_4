964f55939f2848ea51a8fae3c3650bd607898a41
maxud <mlenon13@gmail.com>
2017-08-18 21:05:12 +0300

    новый проект mvc

---
+++ mvc/lesson_4_mvc/lesson_4_mvc/lesson_4_mvc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lesson_4_mvc
+//
+//  Created by maxud on 18.08.17.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
